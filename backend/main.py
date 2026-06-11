329d5f1875ae15259ca66422fe6dd97982a6f56e
ankeshkashyap <ankeshkashyap2222@gmail.com>
2026-06-11 23:35:03 +0530

    implemented task CRUD with postgesql

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 import psycopg2
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Optional
+
 
 app = FastAPI()
 
@@ -11,6 +14,75 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+class TaskCreate(BaseModel):
+    title:str
+
+@app.post("/tasks")
+def create_task(task: TaskCreate):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="studyplanner",
+        user ="postgres",
+        password="0709"
+    )
+    cursor=conn.cursor()
+
+    cursor.execute("INSERT INTO tasks (title,completed) VALUES (%s,%s)",(task.title,False))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"message":"Task created"}
+
+
+
+class TaskUpdate(BaseModel):
+    completed:Optional[bool]= None
+    title:Optional[str]= None
+
+@app.put("/tasks/{id}")
+def update_task(id: int,task: TaskUpdate):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="studyplanner",
+        user ="postgres",
+        password="0709"
+    )
+    cursor=conn.cursor()
+
+    cursor.execute("UPDATE tasks SET title =%s, completed = %s WHERE id= %s",(task.title,task.completed,id))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"message":"Task updated"}
+
+
+
+
+@app.delete("/tasks/{id}")
+def delete_task(id:int):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="studyplanner",
+        user ="postgres",
+        password="0709"
+    )
+    cursor=conn.cursor()
+
+    cursor.execute("DELETE FROM tasks WHERE id = %s",(id,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"message":"Task deleted"}
+    
+
+
 
 @app.get("/")
 def home ():
